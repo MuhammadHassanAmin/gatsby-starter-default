5d8725f6ba23953a2e5efd396ff0abbc0ec57295
Hassan Amin <mhassanamin94@gmail.com>
2021-12-03 00:05:32 +0500

    added images

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,15 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
+      src="../images/code-competition.jpg"
+      width={300}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <StaticImage
+      src="../images/winner.png"
       width={300}
       quality={95}
       formats={["auto", "webp", "avif"]}
